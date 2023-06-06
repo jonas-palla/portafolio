8d53770c8fa01f23b25e519e21d1289256f2fe4b
Jonas progi <128178279+jonas-palla@users.noreply.github.com>
2023-06-06 03:24:17 -0300

    agregar archivo javascript

---
+++ javascript.js
@@ -0,0 +1,10 @@
+let $btnMenu = document.getElementById("menu");
+
+
+function dropdown(params) {
+    
+}
+document.getElementById("menu").addEventListener("click", function() {
+    var menuList = document.getElementById("menu-list");
+    menuList.classList.toggle("show");
+  });
